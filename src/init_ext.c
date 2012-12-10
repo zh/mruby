8c9c58330af5788cc403ec411cefe24dd19ec68e
Tomoyuki Sahara <tsahara@iij.ad.jp>
2012-12-10 13:32:11 +0900

    do in the same way as init.c.

--- src/init_ext.c
+++ src/init_ext.c
@@ -6,58 +6,58 @@
 
 #include "mruby.h"
 
+void mrb_init_require(mrb_state *mrb);
+void mrb_init_errno(mrb_state *mrb);
+void mrb_init_io(mrb_state *mrb);
+void mrb_init_file(mrb_state *mrb);
+void mrb_init_socket(mrb_state *mrb);
+void mrb_init_dir(mrb_state *mrb);
+void mrb_init_process(mrb_state *mrb);
+void mrb_init_digest(mrb_state *mrb);
+void mrb_init_env(mrb_state *mrb);
+void mrb_init_pack(mrb_state *mrb);
+void mrb_init_syslog(mrb_state *mrb);
+void mrb_init_random(mrb_state *mrb);
 void mrb_init_mrblib_ext(mrb_state*);
 
+#define DONE mrb_gc_arena_restore(mrb, 0);
 void
 mrb_init_ext(mrb_state *mrb)
 {
 #ifdef ENABLE_REQUIRE
-  extern void mrb_init_require(mrb_state *mrb);
-  mrb_init_require(mrb);
+  mrb_init_require(mrb); DONE;
 #endif
 #ifdef ENABLE_ERRNO
-  extern void mrb_init_errno(mrb_state *mrb);
-  mrb_init_errno(mrb);
+  mrb_init_errno(mrb); DONE;
 #endif
 #ifdef ENABLE_IO
-  extern void mrb_init_io(mrb_state *mrb);
-  mrb_init_io(mrb);
-  extern void mrb_init_file(mrb_state *mrb);
-  mrb_init_file(mrb);
+  mrb_init_io(mrb); DONE;
+  mrb_init_file(mrb); DONE;
 #ifdef ENABLE_SOCKET
-  extern void mrb_init_socket(mrb_state *mrb);
-  mrb_init_socket(mrb);
+  mrb_init_socket(mrb); DONE;
 #endif
 #endif
 #ifdef ENABLE_DIR
-  extern void mrb_init_dir(mrb_state *mrb);
-  mrb_init_dir(mrb);
+  mrb_init_dir(mrb); DONE;
 #endif
 #ifdef ENABLE_PROCESS
-  extern void mrb_init_process(mrb_state *mrb);
-  mrb_init_process(mrb);
+  mrb_init_process(mrb); DONE;
 #endif
 #ifdef ENABLE_DIGEST
-  extern void mrb_init_digest(mrb_state *mrb);
-  mrb_init_digest(mrb);
+  mrb_init_digest(mrb); DONE;
 #endif
 #ifdef ENABLE_ENV
-  extern void mrb_init_env(mrb_state *mrb);
-  mrb_init_env(mrb);
+  mrb_init_env(mrb); DONE;
 #endif
 #ifdef ENABLE_PACK
-  extern void mrb_init_pack(mrb_state *mrb);
-  mrb_init_pack(mrb);
+  mrb_init_pack(mrb); DONE;
 #endif
 #ifdef ENABLE_SYSLOG
-  extern void mrb_init_syslog(mrb_state *mrb);
-  mrb_init_syslog(mrb);
+  mrb_init_syslog(mrb); DONE;
 #endif
 #ifdef ENABLE_RANDOM
-  extern void mrb_init_random(mrb_state *mrb);
-  mrb_init_random(mrb);
+  mrb_init_random(mrb); DONE;
 #endif
-  mrb_gc_arena_restore(mrb, 0);
 
   mrb_init_mrblib_ext(mrb);
 }
