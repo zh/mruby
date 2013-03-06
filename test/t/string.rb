f780ab84ae190147f5dcfcb4998814973cce6ac6
Akira Yumiyama <yumiyama@iij.ad.jp>
2013-03-06 11:55:06 +0900

    add String#getbyte
    Conflicts:
    
    	src/string.c
    	test/t/string.rb

--- test/t/string.rb
+++ test/t/string.rb
@@ -602,3 +602,15 @@ assert('String#%') do
     "%-5s: %08x" % [ "ID", 123 ] == "ID   : 0000007b" and
     "foo = %{foo}" % { :foo => 'bar' } == "foo = bar"
 end
+
+assert('String#getbyte') do
+  str1 = "hello"
+  bytes1 = [104, 101, 108, 108, 111]
+  assert_equal bytes1[0], str1.getbyte(0)
+  assert_equal bytes1[-1], str1.getbyte(-1)
+  assert_equal bytes1[6], str1.getbyte(6)
+
+  str2 = "\xFF"
+  bytes2 = [0xFF]
+  assert_equal bytes2[0], str2.getbyte(0)
+end
